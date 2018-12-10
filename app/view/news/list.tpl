725cf29f880d7b639423d6f24ae16da044d78c6f
jarodxie <jarodxie@foxmail.com>
2018-12-10 08:11:09 +0800

    中间件 服务等

--- app/view/news/list.tpl
+++ app/view/news/list.tpl
@@ -8,6 +8,7 @@
             {% for item in list %}
                 <li class="item">
                     <a href="{{ item.url }}">{{ item.title }}</a>
+                    {{ helper.relativeTime(item.time) }}
                 </li>
             {% endfor %}
         </ul>
--- config/config.default.js
+++ config/config.default.js
@@ -6,3 +6,21 @@ exports.view = {
         '.tpl': 'nunjucks'
     }
 }
+
+// 添加 news 的配置项
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0'
+}
+
+// add middleware robot
+exports.middleware = [
+    'robot'
+];
+
+// robot's configurations
+exports.robot = {
+    ua: [
+        /Baiduspider/i,
+    ]
+};
